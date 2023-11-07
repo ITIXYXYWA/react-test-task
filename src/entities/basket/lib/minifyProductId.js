788de34c21e2ddda2c917758777818f5f684c0e9
itixyxywa <ukv220@gmail.com>
2023-11-07 05:04:34 +0500

    delete unused

--- src/entities/basket/lib/minifyProductId.js
+++ src/entities/basket/lib/minifyProductId.js
@@ -1,13 +1,3 @@
 export const minifyProductIdToString = ({ productId, colorId, sizeId }) => {
   return `${productId}_${colorId}_${sizeId}`;
 };
-
-export const extractProductIdFromString = (minifiedProductId) => {
-  const joinedProductId = minifiedProductId.join("_");
-
-  return {
-    productId: joinedProductId[0],
-    colorId: joinedProductId[1],
-    sizeId: joinedProductId[2],
-  };
-};
